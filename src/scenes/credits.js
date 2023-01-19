75c66002de5c27731ceece2dde5bc6102e50e363
dragonjet <jetriconew@gmail.com>
2023-01-19 14:56:01 -0800

    Credits update

--- src/scenes/credits.js
+++ src/scenes/credits.js
@@ -6,8 +6,8 @@ class CreditsScene extends Phaser.Scene {
     this.add.rectangle(640, 360, 1240, 680, 0x000000, 0).setStrokeStyle(3, 0x6d3e4b);
 
     // Back
-    this.add.sprite(65, 60, 'ui', 'wings').setScale(0.4);
-    this.add.text(95, 45, 'Back', {
+    this.add.sprite(65, 65, 'ui', 'wings').setScale(0.4);
+    this.add.text(95, 50, 'Back', {
       fontFamily: 'VT323, Arial, Helvetica, sans-serif',
       fontSize: 30,
       align: 'left',
@@ -16,28 +16,28 @@ class CreditsScene extends Phaser.Scene {
     });
 
     // Play Button
-    const playBtn = this.add.rectangle(130, 60, 200, 50, 0xff0000, 0);
+    const playBtn = this.add.rectangle(130, 65, 200, 50, 0xff0000, 0);
     playBtn.setInteractive({ useHandCursor: true })
       .on('pointerdown', () => {
-        this.sound.add('nav', { volume: 0.4 });
+        this.sound.add('nav', { volume: 0.4 }).play();
         this.scene.start('menu');
       });
 
     // Logo
-    this.add.sprite(640, 170, 'ui', 'logo').setScale(0.45);
+    this.add.sprite(930, 190, 'ui', 'logo').setScale(0.5);
 
     // Credits
-    this.add.text(340, 280, 'CREDITS', {
+    this.add.text(220, 40, 'CREDITS', {
       fontFamily: 'VT323, Arial, Helvetica, sans-serif',
-      fontSize: 60,
-      align: 'center',
+      fontSize: 50,
+      align: 'left',
       fixedWidth: 600,
       color: '#6d3e4b',
     });
 
     // Description
-    this.add.rectangle(980, 520, 500, 280, 0x000000, 0).setStrokeStyle(2, 0x6d3e4b);
-    this.add.text(750, 405, `
+    this.add.rectangle(930, 490, 500, 280, 0x000000, 0).setStrokeStyle(2, 0x6d3e4b);
+    this.add.text(720, 375, `
       Birds Banking Frenzy!! is a game produced in Enna's Angelic Teahouse.
       It is a typing game about Enna Alouette receiving red packets from her senpais.
       We hope you enjoy the short project. Happy Lunar New Year!
@@ -52,44 +52,48 @@ class CreditsScene extends Phaser.Scene {
     });
 
     // Contributors
-    this.add.sprite(150, 400, 'ui', 'frame').setScale(0.66);
-    this.add.sprite(150, 400, 'ui', 'av_hazel').setScale(0.4);
-    this.add.text(220, 370, 'Hazel @HazelSokolov', {
+    this.add.sprite(150, 170, 'ui', 'frame').setScale(0.66);
+    this.add.sprite(150, 170, 'ui', 'av_hazel').setScale(0.4);
+    this.add.text(220, 140, 'Hazel @HazelSokolov', {
       fontFamily: 'VT323, Arial, Helvetica, sans-serif',
       fontSize: 30,
       color: '#6d3e4b',
     });
-    this.add.text(220, 400, 'Illustration', {
+    this.add.text(220, 170, 'Illustration', {
       fontFamily: 'VT323, Arial, Helvetica, sans-serif',
       fontSize: 26,
       color: '#6d3e4b',
     });
 
-    this.add.sprite(150, 510, 'ui', 'frame').setScale(0.66);
-    this.add.sprite(150, 510, 'ui', 'av_jetrico').setScale(0.4);
-    this.add.text(220, 480, 'jetrico @jetri_co', {
+    this.add.sprite(150, 280, 'ui', 'frame').setScale(0.66);
+    this.add.sprite(150, 280, 'ui', 'av_jetrico').setScale(0.4);
+    this.add.text(220, 250, 'jetrico @jetri_co', {
       fontFamily: 'VT323, Arial, Helvetica, sans-serif',
       fontSize: 30,
       color: '#6d3e4b',
     });
-    this.add.text(220, 510, 'Programming', {
+    this.add.text(220, 280, 'Programming', {
       fontFamily: 'VT323, Arial, Helvetica, sans-serif',
       fontSize: 26,
       color: '#6d3e4b',
     });
 
-    this.add.sprite(150, 620, 'ui', 'frame').setScale(0.66);
-    this.add.sprite(150, 620, 'ui', 'av_mata').setScale(0.4);
-    this.add.text(220, 590, 'Matahari @mataharigoreng', {
+    this.add.sprite(150, 390, 'ui', 'frame').setScale(0.66);
+    this.add.sprite(150, 390, 'ui', 'av_mata').setScale(0.4);
+    this.add.text(220, 360, 'Matahari @mataharigoreng', {
       fontFamily: 'VT323, Arial, Helvetica, sans-serif',
       fontSize: 30,
       color: '#6d3e4b',
     });
-    this.add.text(220, 620, 'Illustration', {
+    this.add.text(220, 390, 'Illustration', {
       fontFamily: 'VT323, Arial, Helvetica, sans-serif',
       fontSize: 26,
       color: '#6d3e4b',
     });
+
+    this.add.sprite(150, 500, 'ui', 'frame').setScale(0.66);
+
+    this.add.sprite(150, 610, 'ui', 'frame').setScale(0.66);
   }
 }
 
